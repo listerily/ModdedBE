fc327b189cc01b5536e6603e04de398c342ee6da
秋云未云 <listerily@outlook.com>
2020-07-23 19:28:58 +0800

    Launcher

---
+++ Code/app/src/main/java/net/listerily/moddedpepro/launcher/Launcher.java
@@ -0,0 +1,9 @@
+package net.listerily.moddedpepro.launcher;
+
+public class Launcher {
+    public static Launcher mInstance;
+
+    public NModmanager modmanager;
+    public ResourcesManager resManager;
+
+}
---
+++ Code/app/src/main/java/net/listerily/moddedpepro/launcher/NModManager.java
@@ -0,0 +1,7 @@
+package net.listerily.moddedpepro.launcher;
+
+public class NModManager {
+    NModManager()
+    {}
+
+}
