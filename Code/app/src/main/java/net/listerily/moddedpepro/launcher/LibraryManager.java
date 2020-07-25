fbcbc97c1aa4a99b330b653cce4b107d0024dab5
秋云未云 <listerily@outlook.com>
2020-07-25 19:07:24 +0800

    Update LibraryManager.java

--- Code/app/src/main/java/net/listerily/moddedpepro/launcher/LibraryManager.java
+++ Code/app/src/main/java/net/listerily/moddedpepro/launcher/LibraryManager.java
@@ -3,9 +3,17 @@ package net.listerily.moddedpepro.launcher;
 public class LibraryManager {
     private Launcher launcher;
 
+    public String NATIVE_LIBRARY_MINECRAFTPE = "libminecraftpe.so";
+    public String NATIVE_LIBRARY_SUBSTRATE = "libsubstrate.so";
+    public String NATIVE_LIBRARY_CORE = "libcore.so";
+
     public LibraryManager(Launcher launcher) {
         this.launcher = launcher;
     }
 
+    public void extractAllGameNativeLibraries()
+    {
+
+    }
 
 }
