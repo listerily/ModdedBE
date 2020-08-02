d1011a56ed63520d17fc94a5a3fb902395358a57
秋云未云 <listerily@outlook.com>
2020-08-02 19:01:15 +0800

    Add a Toast

--- Code/app/src/main/java/net/listerily/moddedpepro/LoadingActivity.java
+++ Code/app/src/main/java/net/listerily/moddedpepro/LoadingActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import net.listerily.moddedpepro.ui.BitmapRepeater;
 
@@ -20,4 +21,13 @@ public class LoadingActivity extends AppCompatActivity {
         bitmap = BitmapRepeater.repeat(getWindowManager().getDefaultDisplay().getWidth(), getWindowManager().getDefaultDisplay().getHeight(), bitmap);
         getWindow().getDecorView().setBackground(new BitmapDrawable(bitmap));
     }
+
+    @Override
+    public void onBackPressed() {
+        Toast toast = new Toast(this);
+        toast.setText(R.string.app_loading_summary);
+        toast.show();
+    }
+
+
 }
