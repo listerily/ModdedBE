3e741c4a10b0e204193897d277f408490a7d1e22
listerily <listerily@outlook.com>
2021-01-18 18:03:05 +0800

    Added listener to read preferences data

--- ModdedBE-Android/app/src/main/java/net/listerily/moddedbe/OptionsActivity.kt
+++ ModdedBE-Android/app/src/main/java/net/listerily/moddedbe/OptionsActivity.kt
@@ -1,11 +1,13 @@
 package net.listerily.moddedbe
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import net.listerily.endercore.android.EnderCore
 
 class OptionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +26,8 @@ class OptionsActivity : AppCompatActivity() {
     fun onInstallNModsClicked() {}
     fun onInfoClicked() {}
     class OptionsFragment : PreferenceFragmentCompat() {
+        private lateinit var listener: SharedPreferences.OnSharedPreferenceChangeListener
+
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
             findPreference<Preference>("manage")!!.onPreferenceClickListener = Preference.OnPreferenceClickListener {
@@ -38,6 +42,35 @@ class OptionsActivity : AppCompatActivity() {
                 (activity as OptionsActivity?)!!.onInfoClicked()
                 false
             }
+
+            listener =
+                    SharedPreferences.OnSharedPreferenceChangeListener { sharedPreferences: SharedPreferences, key: String ->
+                        run {
+                            when (key) {
+                                "auto_license" -> {
+                                    EnderCore.getInstance().optionsManager.autoLicense = sharedPreferences.getBoolean(key, EnderCore.getInstance().optionsManager.autoLicense)
+                                }
+                                "redirect_directory" -> {
+                                    EnderCore.getInstance().optionsManager.redirectGameDir = sharedPreferences.getBoolean(key, EnderCore.getInstance().optionsManager.redirectGameDir)
+                                }
+                                "use_nmods" -> {
+                                    EnderCore.getInstance().optionsManager.useNMods = sharedPreferences.getBoolean(key, EnderCore.getInstance().optionsManager.useNMods)
+                                }
+                            }
+                        }
+                        EnderCore.getInstance().optionsManager.saveDataToFile()
+                    }
+            preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(listener)
+        }
+
+        override fun onResume() {
+            super.onResume()
+            preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(listener)
+        }
+
+        override fun onPause() {
+            super.onPause()
+            preferenceManager.sharedPreferences.unregisterOnSharedPreferenceChangeListener(listener)
         }
     }
 
