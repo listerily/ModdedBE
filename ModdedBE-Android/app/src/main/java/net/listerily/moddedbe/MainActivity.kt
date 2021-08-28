e8a9343f4453d9efe1738ed53d41578757c8cb33
TripleCamera <TripleCamera@outlook.com>
2021-08-28 17:37:27 +0800

    Configure Gradle & UI  - Disable obsfucation in releases  - Add BUILD_DATE in BuildConfig  - Read VERSION_NAME from BuildConfig

--- ModdedBE-Android/app/src/main/java/net/listerily/moddedbe/MainActivity.kt
+++ ModdedBE-Android/app/src/main/java/net/listerily/moddedbe/MainActivity.kt
@@ -3,12 +3,14 @@ package net.listerily.moddedbe
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.version_name_textview).text = BuildConfig.VERSION_NAME
     }
 
     fun onStartGameClicked(view: View) {
